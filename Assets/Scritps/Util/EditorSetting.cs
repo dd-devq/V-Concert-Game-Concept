c1394204d59afa776309058dcc094569bfbd7027
Danh Nguyen Duc <danh.nguyen1909@hcmut.edu.vn>
2023-10-21 22:14:07 +0700

    Create Humanoid Avatars & Animations

---
+++ Assets/Scritps/Util/EditorSetting.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class EditorSetting : MonoBehaviour
+{
+    private void Awake()
+    {
+#if UNITY_EDITOR
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 30;
+#endif
+    }
+}
