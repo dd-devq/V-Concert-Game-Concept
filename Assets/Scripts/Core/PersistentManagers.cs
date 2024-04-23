71f174e5bba8d69add5cb28933e4230c78e9d772
Danh <dd.devq@gmail.com>
2024-04-24 00:00:02 +0700

    The Rest

--- Assets/Scripts/Core/PersistentManagers.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.AddressableAssets;
-using System.Collections.Generic;
-
-[CreateAssetMenu(menuName = "Persistent Managers")]
-public class PersistentManagers : ScriptableObject
-{
-    public List<AssetReference> listManagers;
-}
